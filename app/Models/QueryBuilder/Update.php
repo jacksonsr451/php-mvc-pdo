ae273f4ac6decbb7227dae6681c841978ba8cdce
jacksonsr451 <jackson.severino.rocha@escola.pr.gov.br>
2022-07-09 15:42:13 -0300

    add update

---
+++ app/Models/QueryBuilder/Update.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\QueryBuilder;
+
+
+class Update {
+    private static array $attributeWhere;
+
+    public static function where($value) {
+        self::$attributeWhere = $value;
+
+        return self::class;
+    }
+
+    public static function SQLUpdate($table, $attributes) {
+        $sql = "UPDATE {$table} set ";
+        unset($attributes[array_keys(self::$attributeWhere)[0]]);
+
+        foreach ($attributes as $key => $value) {
+            $sql += "{$key}" . " :{$key}, ";
+        }
+        $sql = rtrim($sql, ', ');
+        $where = array_keys(self::$attributeWhere);
+        $sql += "WHERE {$where[0]} = :{$where[0]}";
+        return $sql;
+    }
+}
--- app/Models/Traits/PersistDB.php
+++ app/Models/Traits/PersistDB.php
@@ -3,14 +3,20 @@
 namespace App\Models\Traits;
 
 use App\Http\QueryBuilder\Insert;
+use App\Http\QueryBuilder\Update;
 
 trait PersistDB {
 
-    public function insert($atributes) {
-        $sql = Insert::SQLInsert($this->table, $atributes);
+    public function insert($attributes) {
+        $sql = Insert::SQLInsert($this->table, $attributes);
         $insert = $this->connect->prepare($sql);
-        return $insert->execute($atributes);
+        return $insert->execute($attributes);
     }
 
-    public function update() {}
+    public function update($attributes, $where) {
+        $sql = Update::where($where)::SQLUpdate($this->table, $attributes);
+        $update = $this->connect->prepare($sql);
+        $update->execute($attributes);
+        return $update->rowCount();
+    }
 }
