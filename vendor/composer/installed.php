70798e930680bc400e9560daf622baccb8124393
jacksonsr451 <jackson.severino.rocha@escola.pr.gov.br>
2022-07-12 23:06:55 -0300

    Update composer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '3405f8ad5c7aefcca2205db6699d2bda1dfd4d7c',
+        'reference' => '037daaa71c551821b6f5182cee12deea77bd3548',
         'name' => 'jacksonsr45/php-mvc-pdo',
         'dev' => true,
     ),
@@ -16,7 +16,7 @@
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '3405f8ad5c7aefcca2205db6699d2bda1dfd4d7c',
+            'reference' => '037daaa71c551821b6f5182cee12deea77bd3548',
             'dev_requirement' => false,
         ),
         'squizlabs/php_codesniffer' => array(
